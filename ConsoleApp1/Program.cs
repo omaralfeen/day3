256f17933b304f6b9d1c3ca1c7a5eb2d2693ab48
FORMAT <FORMAT@DESKTOP-E2BEAN4>
2024-07-13 14:10:03 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace day3
+{
+    class student(int id,string name,int age)
+    {
+        /*
+        int id;
+        string name;
+        int age;
+
+        public student(int Id,string Name,int Age)
+        {
+            id = Id;
+            name = Name;
+            age = Age;
+
+        }
+        */
+        static student()
+        {
+            Console.WriteLine("hello");
+        }
+
+        /*
+        public void getDate(int Id, string Name, int Age)
+        {
+            id = Id;
+            name = Name;
+            age = Age;
+        }
+        */
+
+        public void setDate()
+        {
+            Console.WriteLine($" {id} - {name} - {age}");
+        }
+        public string isGradute()
+        {
+            if (age < 24)
+                return $"{name} is gradute";
+            else
+                return $"{name} is not gradute";
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                student stu1 = new student(1,"omar",21);
+                student stu2 = new student(2, "ali", 20);
+                // stu1.getDate(1, "omar", 20);
+                stu1.setDate();
+                stu2.setDate();
+                Console.WriteLine(stu1.isGradute());
+                ////
+                ////
+                ///
+ 
+            }
+        }
+    }
+    
+}
---
+++ ConsoleApp1/lab3.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+namespace lab3
+{
+    class Omar
+    {
+        static void Main()
+        {
+
+            Book bo1 = new Book(1, "omar", "action", "2005", "2225", true);
+            Book bo2 = new Book(2, "omar", "action", "2005", "2225", true);
+            bo1.printt();
+
+            Console.WriteLine(bo1.Deleted());
+            Console.WriteLine(Book.count);
+        }
+    }
+ 
+    class Book
+    {
+        int id;
+        string name;
+        string type;
+        string createdon;
+        bool isDeleted;
+        string price;
+        public static int count = 0;
+
+        public Book(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+
+        }
+        public Book(string type, string createdon)
+        {
+            this.type = type;
+            this.createdon = createdon;
+        }
+        public Book(int id, string name, string type, string createdon, string price, bool isDeleted)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.createdon = createdon;
+            this.price = price;
+            this.isDeleted = isDeleted;
+            count++;
+
+        }
+        public void printt()
+        {
+            Console.WriteLine($"{id} - {name} - {type} - {createdon} - {price} - {isDeleted}");
+        }
+        public string Deleted()
+        {
+            return isDeleted ? "book is deleted" : "book is not deleted";
+        }
+
+    }
+
+}
+
